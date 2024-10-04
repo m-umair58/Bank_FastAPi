afc258ad61e30a006f1cf46eaa32dfde37e91bd2
Muhammad Umair <mianumair1958@gmail.com>
2024-10-04 21:00:53 +0500

    Added transaction limits

--- db_queries/transaction_queries.py
+++ db_queries/transaction_queries.py
@@ -1,5 +1,7 @@
 from models.transaction_model import Transaction
 from sqlalchemy.orm import Session
+from sqlalchemy import or_,func
+from datetime import date
 
 class transaction_queries:
     def get_transaction_by_id(t_id,db:Session):
@@ -8,3 +10,12 @@ class transaction_queries:
     def add_transaction(transaction,db:Session):
         db.add(transaction)
         db.commit()
+
+    def get_transaction_by_user_id(user_id, date_today, db: Session):
+        return db.query(Transaction).filter(
+            or_(
+                Transaction.senders_acc_id == user_id,
+                Transaction.receivers_acc_id == user_id
+            ),
+            func.date(Transaction.date) == date_today
+        ).all()
--- services/transaction_services.py
+++ services/transaction_services.py
@@ -1,31 +1,50 @@
-from schemas.transaction_schema import Create_Transaction
 from db_queries.transaction_queries import transaction_queries
-from schemas.account_schema import AccountCreate,AccountGet
+from schemas.account_schema import AccountGet
 from db_queries.account_queries import account_queries
+from db_queries.account_limit_queries import account_limit_queries
 from fastapi import HTTPException
 from starlette import status
 from models.transaction_model import Transaction
-from fastapi import Depends
-from oauth2 import get_user_info
-from sqlalchemy.orm import Session
-from database import get_db
+from datetime import date
+
+def total_transactions(transactions):
+        total_amount=0
+        for transaction in transactions:
+            total_amount += transaction.amount
+
+        return total_amount
 
 class TransactionServices:
-    def create_transaction(t_type,amount,receiver:int|None=None,user_data=Depends(get_user_info),db:Session=Depends(get_db)):
-        if t_type=='deposit' or t_type=='withdraw' and receiver:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="There Should be no receiver id here")
+    
+    def create_transaction(t_type,amount,receiver,user_data,db):
+        if t_type=='deposit' or t_type=='withdraw' :
+            if receiver:
+                raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="There Should be no receiver id here")
 
         if t_type=='transfer' and receiver<=0:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="You need put a valid receiver account id!")
         if t_type=='transfer' and receiver is None:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="You need put a valid receiver account id!")
 
-        if user_data['acc_id']==0:
+        if user_data["acc_id"]==0:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="You need to create an account first!")
-        account_id=account_queries.get_account_by_acc_id(user_data['acc_id'],db)
         if user_data['acc_id']==receiver:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="You cannot send money to the same account!")
+        # above all are checks
 
+        # check the account type
+        account_details= account_queries.get_account_by_acc_id(user_data["acc_id"],db)
+        account_limit_details = account_limit_queries.get_account_limit_by_type(account_details.acc_type,db)
+
+        # to check daily transaction limit
+        transactions = transaction_queries.get_transaction_by_user_id(user_data['acc_id'],date.today(),db)
+
+        total_transaction_amount = total_transactions(transactions)
+        print("*********",total_transaction_amount,"************")
+        if account_limit_details.daily_limit<total_transaction_amount+amount:
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail="You have crossed your daily transaction limit!")
+
+        account_id=account_queries.get_account_by_acc_id(user_data['acc_id'],db)
         if t_type=="deposit":
             get_acc:AccountGet=account_queries.get_account_by_acc_id(account_id.acc_id,db)
             get_acc.acc_balance+=amount
